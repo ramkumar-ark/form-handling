ff011d6152b5c7a5bd1f94c5f0524957ed0be78c
Ram Kumar <ramgambit@gmail.com>
2025-06-10 15:28:19 +0530

    implemented form controls and validations

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)],
+    }),
+  });
+
+  onSubmit() {
+    console.log(this.form);
+    const email = this.form.value.email;
+    const password = this.form.value.password;
+    console.log(`Email: ${email}, Password: ${password}`);
+    console.log(this.form.get('email'));
+  }
+}
